9875a11677345829a960468769a1a4bc0f7bd2c1
hipsmaro <hk160628@ezweb.ne.jp>
2023-06-26 18:21:30 +0900

    setstateだけ

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import {
   IconButton,
   Icon,
 } from "native-base";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
 
 const Example = () => {
   return (
@@ -16,9 +16,9 @@ const Example = () => {
         backgroundColor="tertiary.100"
         flex="1"
         alignItems="center"
-        py="24"
-        pl="8"
-        pr="8"
+        pt="20"
+        pb="32"
+        px="8"
       >
         <IconButton
           icon={
@@ -30,15 +30,30 @@ const Example = () => {
         ></IconButton>
 
         <Heading fontSize="32">タスク詳細</Heading>
-        <Box bgColor="white" w="100%" h="80%" py="4" px="4" m="4">
+        <Box bgColor="white" w="100%" h="80%" py="4" px="4" m="4" rounded="10%">
           <Heading>買い物に行く</Heading>
-          <Text>05/01 18:00まで</Text>
+          <Text color="muted.500">05/01 18:00まで</Text>
           <Text></Text>
-          <Heading fontSize="24">・牛乳</Heading>
-          <Heading>・にんじん</Heading>
+          <Heading fontSize="18" pt="2">
+            ・牛乳
+          </Heading>
+          <Heading fontSize="18">・にんじん</Heading>
+          <IconButton
+            icon={
+              <Icon
+                as={<FontAwesome name="pencil-square-o" />}
+                name="add task"
+                color="black"
+                size="8"
+              />
+            }
+            position="absolute"
+            top="2"
+            right="2"
+          ></IconButton>
         </Box>
         <IconButton
-          bg="tertiary.500"
+          bg="tertiary.400"
           rounded="100%"
           size="16"
           icon={
