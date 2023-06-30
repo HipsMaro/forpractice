191809d0d99d14d3300063bab40537f9c9f7d8dc
hipsmaro <hk160628@ezweb.ne.jp>
2023-06-30 20:35:36 +0900

    ざらしと連携

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   NativeBaseProvider,
   Box,
@@ -9,13 +9,33 @@ import {
   Input,
   TextArea,
 } from "native-base";
+
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
 
+import { createClient } from "@supabase/supabase-js";
+import "react-native-url-polyfill/auto";
+
+const supabaseUrl = "https://twwzezmtrtkprezyhsac.supabase.co/";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3d3plem10cnRrcHJlenloc2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5MzkzMjksImV4cCI6MjAwMzUxNTMyOX0.kZjEoXgbh4xgp6O15sHDA2YlugALzPqtfg1Y6wJsB78";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 const Example = () => {
-  const [text, setText] = useState("買い物に行く");
-  const [date, setDate] = useState("05/01 18:00まで");
-  const [task, setTask] = useState("・牛乳");
+  const [task_name, setTask_name] = useState("");
+  const [task_deadline, setTask_deadline] = useState("");
+  const [task_detail, setTask_detail] = useState("");
   const [isReadOnly, setIsReadOnly] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from("tasks").select();
+      const taskName = data[4].task_name;
+      setTask_name(taskName);
+      setTask_deadline(data[4].task_deadline);
+      setTask_detail(data[4].task_detail);
+    };
+    getData();
+  }, []);
 
   return (
     <NativeBaseProvider>
@@ -39,25 +59,30 @@ const Example = () => {
         <Heading fontSize="32">タスク詳細</Heading>
         <Box bgColor="white" w="100%" h="80%" py="4" px="4" m="4" rounded="10%">
           <Input
+            size="2xl"
             isReadOnly={isReadOnly}
             variant="unstyled"
             borderWidth={isReadOnly ? "0" : "1"}
-            value={text}
-            onChangeText={(text) => setText(text)}
+            value={task_name}
+            onChangeText={(text) => setTask_name(text)}
           />
+
           <Input
+            size="xs"
             isReadOnly={isReadOnly}
             variant="unstyled"
             borderWidth={isReadOnly ? "0" : "1"}
-            value={date}
-            onChangeText={(date) => setDate(date)}
+            value={task_deadline}
+            onChangeText={(text) => setTask_deadline(text)}
           />
+
           <TextArea
+            size="md"
             isReadOnly={isReadOnly}
             variant="unstyled"
             borderWidth={isReadOnly ? "0" : "1"}
-            value={task}
-            onChangeText={(task) => setTask(task)}
+            value={task_detail}
+            onChangeText={(text) => setTask_detail(text)}
           />
 
           <IconButton
